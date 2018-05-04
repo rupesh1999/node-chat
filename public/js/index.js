1136e7092eb4ca46d747f033238ff8b08b397e56
rupesh1999 <31174728+rupesh1999@users.noreply.github.com>
2018-05-04 18:55:52 +0530

    added templating engine with formatted messages

--- public/js/index.js
+++ public/js/index.js
@@ -4,11 +4,19 @@ socket.on('connect' , function() {
     console.log('new user connected');
 })
 socket.on('newMessage' , function(message) {
-    console.log('new message' , message);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template =$('#message-template').html();
+    var html = Mustache.render(template , {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
+    $('#messages').append(html);
+  /*  console.log('new message' , message);
     var li = $('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+    li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
-    $('#messages').append(li);
+    $('#messages').append(li);*/
 });
 
 socket.on('disconnect' , function() {
@@ -16,12 +24,22 @@ socket.on('disconnect' , function() {
 });
 
 socket.on('newLocationMessage' , function(message) {
-    var li= $('<li></li>');
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+
+    var template =$('#location-message-template').html();
+    var html = Mustache.render(template , {
+        url: message.url,
+        from: message.from,
+        createdAt: formattedTime
+    });
+    $('#messages').append(html);
+
+    /* var li= $('<li></li>');
     var a = $('<a target="_blank">My Current Location</a>');
 
     li.text(`${message.from}: `);
     a.attr('href', message.url);
-    li.append(a);
+    li.append(a);*/
 
     $('#messages').append(li);
 });
